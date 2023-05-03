fa099d6a176f7ef309acbcc09c9d3681b8a7d4c6
Rafael Duarte Pereira <rafaelduarte1234.2015@gmail.com>
2023-05-03 14:39:01 -0300

    mudança nos graficos

--- analiser.py
+++ analiser.py
@@ -2,11 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import ttest_ind
+from matplotlib.ticker import ScalarFormatter
 
 df = pd.read_csv('dados-pr-filtrado.csv')
-
-#Trocar closed e merged por 1 ou 0 para fazer os testes de correlação
-df['status'] = df['state'].replace(['MERGED', 'CLOSED'], [1, 0])
 
 #Mudar o tipo das datas
 df['created_at'] = pd.to_datetime(df['created_at'])
@@ -15,16 +13,19 @@ df['closed_at'] = pd.to_datetime(df['closed_at'])
 #Calcular o tempo de vida em horas
 df['tempo'] = (df['closed_at'] - df['created_at']).dt.total_seconds() / 3600
 
+#Definir as dimensões analisadas
 metrics = ['tamanho', 'interacoes', 'descricao', 'tempo']
+
+# Scatterplots
 for m in metrics:
     title = f'{m} X reviews'
     sns.pairplot(data=df, y_vars='reviews', x_vars=m, kind="reg", height=10)
-    plt.xscale('log')
-    plt.yscale('log')
     plt.title(title)
+    plt.yscale('log')
     plt.savefig(f'figs/{title}.png')
     plt.show()
 
+#Boxplots
 for m in metrics:
     title = f'{m} X state'
     groups = df.groupby(['state'])
@@ -33,6 +34,9 @@ for m in metrics:
 
     t_stat, p_valor = ttest_ind(grupo1, grupo2)
     medians = groups[m].median()
+    q1_merged, q3_merged = grupo1.quantile([0.25, 0.75])
+    q1_closed, q3_closed = grupo2.quantile([0.25, 0.75])
+
     sns.boxplot(
         data=df,
         y=m,
@@ -40,20 +44,24 @@ for m in metrics:
         width=0.8,
         linewidth=1.0,
     )
-    # plt.xscale('log')
-    plt.yscale('log')
+
+    plt.text(0, q1_merged, f'Q1={q1_merged:.2f}', horizontalalignment='left')
+    plt.text(1, q1_closed, f'Q1={q1_closed:.2f}', horizontalalignment='left')
+    plt.text(0, q3_merged, f'Q3={q3_merged:.2f}', horizontalalignment='left')
+    plt.text(1, q3_closed, f'Q3={q3_closed:.2f}', horizontalalignment='left')
     for i, val in enumerate(medians.values[::-1]):
         plt.text(i,
                  val,
                  f'{val:.2f}',
                  horizontalalignment='center',
                  fontweight='bold')
+
     plt.title(title + f'\nT-stat: {t_stat:.2f} | P-value: {p_valor:.3f}')
+    plt.yscale('log')
     plt.savefig(f'figs/{title}.png')
     plt.show()
-    
 
-
+#Heatmap de correlações
 cols = ['tamanho', 'interacoes', 'descricao', 'tempo', 'reviews']
 corr = df[cols].corr('spearman')
 
@@ -68,4 +76,3 @@ sns.heatmap(data=corr, annot=True, square=True, robust=False)
 plt.title('Heatmap correlação reviews')
 plt.savefig('figs/heatmap.png')
 plt.show()
-
